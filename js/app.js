82ce67415c3352278a67d22b6d13e949d9a96e4d
philtreez <philipp.treezy@gmail.com>
2025-01-14 01:50:15 +0100

    n

--- js/app.js
+++ js/app.js
@@ -69,11 +69,11 @@ async function setup() {
         }
 
         function updateSliderVisual(sliderDiv, frameIndex) {
-            const frameHeight = 100; // Höhe eines einzelnen Frames
+            const frameHeight = 100; // Höhe eines Frames
             const yOffset = frameIndex * frameHeight; // Berechne Y-Versatz basierend auf dem Frame-Index
             sliderDiv.style.backgroundPosition = `0 -${yOffset}px`;
             console.log(`Setting background position to: 0 -${yOffset}px`);
-        }        
+        }               
 
         // ------ Audio- und Analyser-Node verbinden ------
         const analyserNode = context.createAnalyser();
